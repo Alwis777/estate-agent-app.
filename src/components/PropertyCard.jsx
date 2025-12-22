1f2b77b08679f6230c6481905fabcc5f5f3dc935
Alwis77 <isurialwis132@gmail.com>
2025-12-23 00:05:13 +0530

    Add initial PropertyCard CSS styles

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,25 +1,50 @@
 import { Link }from 'react-router-dom';
 import {FaHeart } from 'react-icons/fa';
 
-const PropertyCard = ({ property }) => {
-    return (
-        <div className="property-card">
-            <div className="property-image">
-                <img src={property.image} alt={property.title} />
+function PropertyCard({ property }) {
+
+
+const formattedPrice = new Intl.NumberFormat('en-GB', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 0,
+}).format(property.price);
+
+return(
+    <div className="property-card">
+      <div className = "card-image-container">
+        <img src = {property.picture} alt ={property.location} className ="card-image"/>
+
+       
+       <button className="favorite-button" title ="Add to Favorites">
+        <FaHeart/>
+         </button>
+        </div>
+
+        <div className="card-content">
+            <div className="card-header">
+                <span className="property-type">{property.type}</span>
+                <span className="property-price">{formattedPrice}</span>
             </div>
-            <div className="property-details">
-                <h2>{property.title}</h2>
-                <p>{property.description}</p>
-                <span className="property-price">${property.price}</span>
-                <Link to={`/properties/${property.id}`} className="view-details">
-                    View Details
-                </Link>
-                <button className="favorite-button">
-                    <FaHeart />
-                </button>
+
+            <h4 className =" card-location">{property.location}</h4>
+
+            <p className="card-description">{property.description.subsctring(0,60)}...               
+            </p>
+
+            <div className="card-footer">
+                <span className= "specs">{property.bedrooms} Beds </span>
+            <Link to ={'/property/${property.id}'} className="view-details.btn">View Details
+            </Link>
             </div>
         </div>
-    );
-};
+    </div>
+);
+}export default PropertyCard;
+            
+      
 
-export default PropertyCard;
+
+
+    
+
