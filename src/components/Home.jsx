81ff986304631765300e59253f5bdbc10c5ef049
Alwis77 <isurialwis132@gmail.com>
2025-12-24 19:30:22 +0530

    feat: add initial Home component file

---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+import { Link } from 'react-router-dom';
+import { FaUserShield, FaBolt, FaAward, FaHandHoldingHeart } from 'react-icons/fa';
+import PropertyCard from './PropertyCard';
+import propertiesData from '../data/properties.json'; 
+import './Home.css';
+
