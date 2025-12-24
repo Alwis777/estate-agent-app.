5c31d91ea4c1b72ade779327b97c0194ee187fc9
Alwis77 <isurialwis132@gmail.com>
2025-12-24 14:54:36 +0530

    Refactor Navbar component and styles: enhance layout, add favorites count badge, and improve responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
-import {Link}from 'react-router-dom';
+import {Link,useLocation}from 'react-router-dom';
+import { FaHome, FaHeart } from 'react-icons/fa';
 import './Navbar.css';
 
-function Navbar(){
+function Navbar({favoritesCount = 0}){
+    const location = useLocation();
     return(
-        <nav className="navbar">
-            <div className="nav-brand">
-                <Link to="/">Estate Agent</Link>
-            </div>
-            <div className="nav-links">
-                <Link to="/">Search Properties</Link>
-                <Link to="/favorites">My Favorites</Link>
+       <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    <FaHome className="logo-icon" />
+                    EstateAgent
+                </Link>
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-links">
+                            Search Properties
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/favorites" className="nav-links favorites-link">
+                            My Favorites
+                            {favoritesCount > 0 && (
+                                <span className="fav-badge">{favoritesCount}</span>
+                            )}
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
-    )
+    );
 }
+
 export default Navbar;
